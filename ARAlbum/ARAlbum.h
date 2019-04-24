48d03149bddf17bb3f6a55ef089c799332a439a3
ccnyou <ccnyou@qq.com>
2019-04-24 20:46:26 +0800

    + target

---
+++ ARAlbum/ARAlbum.h
@@ -0,0 +1,13 @@
+//
+//  ARAlbum.h
+//  ARAlbum
+//
+//  Created by ervinchen on 2019/4/24.
+//  Copyright © 2019 HeiKki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ARAlbum : NSObject
+
+@end
---
+++ ARAlbum/ARAlbum.m
@@ -0,0 +1,13 @@
+//
+//  ARAlbum.m
+//  ARAlbum
+//
+//  Created by ervinchen on 2019/4/24.
+//  Copyright © 2019 HeiKki. All rights reserved.
+//
+
+#import "ARAlbum.h"
+
+@implementation ARAlbum
+
+@end
